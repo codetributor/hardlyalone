1e1ee99d6dd65b86a9957f41397c192b87f140d3
Clint Oka <okaclint@gmail.com>
2024-02-20 21:09:30 -1000

    quote brene brown

--- components/SectionThree.tsx
+++ components/SectionThree.tsx
@@ -2,9 +2,9 @@ export default function SectionThree() {
     return(
         <div className="py-4 flex bg-slate-300 justify-center my-10">
             <div className="sm:max-w-[400px] text-gray-8 w-full lg:max-w-[600px] px-2 text-center py-4">
-                <p>"don't walk through the world looking for evidence that you don't belong, because you will always find it.</p>
-                <p>don't walk through the world looking for evidence that you are not enough, because you will always find it.</p>
-                <p>Our worth and beloning are not negotiated with other people. We carry those inside our hearts." - Brene Brown</p>
+                <p>do not walk through the world looking for evidence that you do not belong, because you will always find it.</p>
+                <p>do not walk through the world looking for evidence that you are not enough, because you will always find it.</p>
+                <p>Our worth and beloning are not negotiated with other people. We carry those inside our hearts. - Brene Brown</p>
             </div>
         </div>
     )
