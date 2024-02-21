17bd20d34c6e9f7cbe662ec87a4f663d51413df7
Clint Oka <okaclint@gmail.com>
2024-02-20 15:49:28 -1000

    add Kaneohe

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
                     <h3 className="font-bold text-2xl">ALL ONE COMMUNITY</h3>
                 </div>
                 <div className="text-gray-400 flex items-center space-x-1">
-                    <p>loneliness project | worldwide</p>
+                    <p>loneliness project | Kaneohe, Hawaii</p>
                     <div>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="red" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
